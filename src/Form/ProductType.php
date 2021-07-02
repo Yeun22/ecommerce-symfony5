33e1978fb7e5b07b290231cec902a4dfaed66954
yeun22 <yeun.lefloch@gmail.com>
2021-07-02 11:28:48 +0200

    Decouvertes des evenements et autres focntionalité de form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,16 +2,20 @@
 
 namespace App\Form;
 
-use App\Entity\Category;
 use App\Entity\Product;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Category;
+use App\Form\DataTransformer\CentimesTransformer;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
 
 class ProductType extends AbstractType
 {
@@ -20,7 +24,7 @@ class ProductType extends AbstractType
         $builder
             ->add('name', TextType::class, ['label' => 'Nom Du Produit', 'attr' => ['placeholder' => 'Tapez le nom du produit']])
             ->add('shortDescription', TextareaType::class, ['label' => 'Description Du Produit', 'attr' => ['placeholder' => 'Donner une courte description mais parlante pour le visiteur']])
-            ->add('price', MoneyType::class, ['label' => 'Prix Du Produit', 'attr' => ['placeholder' => 'Le prix du produit en €']])
+            ->add('price', MoneyType::class, ['label' => 'Prix Du Produit', 'attr' => ['placeholder' => 'Le prix du produit en €'], 'divisor' => 100])
             ->add('mainPicture', UrlType::class, [
                 'label' => "Image du produit",
                 'attr' => ['placeholder' => "Taper une url d'image "]
@@ -30,7 +34,34 @@ class ProductType extends AbstractType
                 'placeholder' => '--Choisir une categorie--',
                 'class' => Category::class,
                 'choice_label' => 'name'
-            ]);;
+            ]);
+
+        // $builder->get('price')->addModelTransformer(new CentimesTransformer);
+
+
+        // $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+        //     $product = $event->getData();
+        //     if ($product->getPrice() != null) {
+        //         $product->setPrice($product->getPrice() * 100);
+        //     }
+        // });
+
+        // $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+        //     $form = $event->getForm();
+        //     /** @var Product */
+        //     $product = $event->getData();
+        //     if ($product->getPrice() != null) {
+        //         $product->setPrice($product->getPrice() / 100);
+        //     }
+        //     // if ($product->getId() === null) {
+        //     //     $form->add('category', EntityType::class, [
+        //     //         'label' => 'Categorie',
+        //     //         'placeholder' => '--Choisir une categorie--',
+        //     //         'class' => Category::class,
+        //     //         'choice_label' => 'name'
+        //     //     ]);
+        //     // }
+        // });
     }
 
     public function configureOptions(OptionsResolver $resolver)
---
+++ src/Form/Type/PriceType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use App\Form\DataTransformer\CentimesTransformer;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+class PriceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        if ($options['divide'] === false) {
+            return;
+        }
+        $builder->addModelTransformer(new CentimesTransformer);
+    }
+    public function getParent()
+    {
+        return NumberType::class;
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'divide' => true
+        ]);
+    }
+}
